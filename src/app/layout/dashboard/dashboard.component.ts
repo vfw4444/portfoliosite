a7c92da19612179fd031ab48d24201c9eece0a26
vfw4444 <vfw4444@gmail.com>
2017-09-01 11:53:15 +0800

    src 1st load

---
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
+
+@Component({
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss'],
+    animations: [routerTransition()]
+})
+export class DashboardComponent implements OnInit {
+    public alerts: Array<any> = [];
+    public sliders: Array<any> = [];
+
+    constructor() {
+        this.sliders.push({
+            imagePath: 'assets/images/slider1.jpg',
+            label: 'SMARTiMarketing.com',
+            text: 'All in One- CRM - Automated SMS, Email, IVR iMarketing'
+        }, {
+            imagePath: 'assets/images/slider2.jpg',
+            label: 'My CRM',
+            text: 'Online Data Reports '
+        }, {
+            imagePath: 'assets/images/slider3.jpg',
+            label: 'Integration',
+            text: 'Cloud Infrastructure'
+        });
+
+        this.alerts.push({
+            id: 1,
+            type: 'success',
+            message: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptates est animi quibusdam praesentium quam, et perspiciatis,
+                consectetur velit culpa molestias dignissimos
+                voluptatum veritatis quod aliquam! Rerum placeat necessitatibus, vitae dolorum`
+        }, {
+            id: 2,
+            type: 'warning',
+            message: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptates est animi quibusdam praesentium quam, et perspiciatis,
+                consectetur velit culpa molestias dignissimos
+                voluptatum veritatis quod aliquam! Rerum placeat necessitatibus, vitae dolorum`
+        });
+    }
+
+    ngOnInit() {
+    }
+
+    public closeAlert(alert: any) {
+        const index: number = this.alerts.indexOf(alert);
+        this.alerts.splice(index, 1);
+    }
+}
